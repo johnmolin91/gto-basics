cb531ed187fcfb0cf901767ac0a87b2682a2d100
johnmolin91 <john.molin91@gmail.com>
2019-04-17 18:33:45 -0400

    all hand combos

--- javascript/main.js
+++ javascript/main.js
@@ -57,7 +57,7 @@ var s2 = 13;
 // var allCards = ['cA', 'dA', 'hA', 'sA', 'cK', 'dK', 'hK', 'sK', 'cQ', 'dQ', 'hQ', 'sQ', 'cJ', 'dJ', 'hJ', 'sJ', 'cT', 'dT', 'hT', 'sT', 'c9', 'd9', 'h9', 's9', 'c8', 'd8', 'h8', 's8', 'c7', 'd7', 'h7', 's7', 'c6', 'd6', 'h6', 's6', 'c5', 'd5', 'h5', 's5', 'c4', 'd4', 'h4', 's4', 'c3', 'd3', 'h3', 's3', 'c2', 'd2', 'h2', 's2'];
 
 function deal() {
-    var allCards = ['aAc', 'aAd', 'aAh', 'aAs', 'bKc', 'bKd', 'bKh', 'bKs', 'cQc', 'cQd', 'cQh', 'cQs', 'dJc', 'dJd', 'dJh', 'dJs', 'eTc', 'eTd', 'eTh', 'eTs', '9c', '9d', '9h', '9s', '8c', '8d', '8h', '8s', '7c', '7d', '7h', '7s', '6c', '6d', '6h', '6s', '5c', '5d', '5h', '5s', '4c', '4d', '4h', '4s', '3c', '3d', '3h', '3s', '2c', '2d', '2h', '2s'];
+    var allCards = ['aAc', 'aAd', 'aAh', 'aAs', 'bKc', 'bKd', 'bKh', 'bKs', 'cQc', 'cQd', 'cQh', 'cQs', 'dJc', 'dJd', 'dJh', 'dJs', 'eTc', 'eTd', 'eTh', 'eTs', 'f9c', 'f9d', 'f9h', 'f9s', 'g8c', 'g8d', 'g8h', 'g8s', 'h7c', 'h7d', 'h7h', 'h7s', 'i6c', 'i6d', 'i6h', 'i6s', 'j5c', 'j5d', 'j5h', 'j5s', 'k4c', 'k4d', 'k4h', 'k4s', 'l3c', 'l3d', 'l3h', 'l3s', 'm2c', 'm2d', 'm2h', 'm2s'];
     var hand = [];
     var ourFirst = allCards[Math.floor(Math.random() * 52)];
     allCards.splice(allCards.indexOf(ourFirst), 1);
@@ -65,44 +65,86 @@ function deal() {
     var ourSecond = allCards[Math.floor(Math.random() * 51)];
     allCards.splice(allCards.indexOf(ourSecond), 1);
     hand.push(ourSecond);
-    console.log(allCards);
+    // console.log(allCards);
     console.log(ourFirst, ourSecond);
 
     var flopOne = allCards[Math.floor(Math.random() * 50)];
     allCards.splice(allCards.indexOf(flopOne), 1);
     hand.push(flopOne);
-    console.log(allCards);
+    // console.log(allCards);
 
     var flopTwo = allCards[Math.floor(Math.random() * 49)];
     allCards.splice(allCards.indexOf(flopTwo), 1);
     hand.push(flopTwo);
-    console.log(allCards);
+    // console.log(allCards);
 
     var flopThree = allCards[Math.floor(Math.random() * 48)];
     allCards.splice(allCards.indexOf(flopThree), 1);
     hand.push(flopThree);
-    console.log(allCards);
+    // console.log(allCards);
 
     console.log(flopOne, flopTwo, flopThree);
 
     var turn = allCards[Math.floor(Math.random() * 47)];
     allCards.splice(allCards.indexOf(turn), 1);
     hand.push(turn);
-    console.log(allCards);
+    // console.log(allCards);
     
     console.log(turn);
 
     var river = allCards[Math.floor(Math.random() * 46)];
     allCards.splice(allCards.indexOf(river), 1);
     hand.push(river);
-    console.log(allCards);
+    // console.log(allCards);
 
     console.log(river);
 
-    console.log(hand);
+    // console.log(hand);
     hand.sort();
-    hand.reverse();
     console.log(hand);
+
+    function swap(cards, i, j) {
+        var tmp = cards[i];
+        cards[i] = cards[j];
+        cards[j] = tmp;
+    }
+    
+    function getCombos(input) {
+        var counter = [],
+            combos = [],
+            cards = input.slice(0, 7),
+            length = cards.length,
+            fiveCardHand = [],
+            i;
+    
+        for (i = 0; i < length; i++) {
+            counter[i] = 0;
+        }
+    
+        combos.push(input);
+        i = 0;
+        while (i < length) {
+            if (counter[i] < i) {
+                swap(cards, i % 2 === 1 ? counter[i] : 0, i);
+                counter[i]++;
+                i = 0;
+                combos.push(cards.join(' '));
+            } else {
+                counter[i] = 0;
+                i++;
+            }
+        }
+        // console.log(combos);
+        for (i = 0; i < combos.length; i++) {
+            var cardString = combos[i].toString();
+            fiveCardHand.push(cardString.slice(0, 19));
+            i++;
+        };
+        console.log(fiveCardHand);
+        return combos;
+    };
+
+    getCombos(hand);
 };
 
 deal();
